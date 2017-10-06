14d0e81f837c53c13477996741abf4dc51e31d7f
weverson10 <weverson_acioly@hotmail.com>
2017-10-06 16:21:04 -0300

    fiz a página de redefinir a senha

---
+++ projetoramon/redefinir.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html lang="pt-br">
+
+<html>
+	<head>
+		<title>Redefinir Senha</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/redefinir.css">
+		<link rel="icon" href="img/icon.png">
+	</head>
+
+	<body>
+		<!-- ======== Cabeçalho ========== -->
+		<header id="cabecalho">
+			
+		</header><!-- ======== Fim do cabeçalho ==========-->
+
+		<div style="width: 400px;height: 400px;background: #ffffff;margin: 80px auto 0px auto;border-radius: 8px;">
+
+			<div style="width: 300px;height: auto;margin: 0 auto;padding-top:100px;">
+
+
+				<form >
+
+				<h3 style="text-align: center;">Nome do Usuário</h3><br><br>
+
+
+				<input type="password" name="senha" style="width:300px;height: 30px;border-radius: 4px;" placeholder="Digite uma nova senha" minlength="8"><br><br>
+				<input type="password" name="confirmar senha" style="width:300px;height: 30px;border-radius: 4px;"  placeholder="Confirme a senha" minlength="8"><br><br><br>
+				
+
+				<div style="width: 150px;height: 50px;margin: 0px auto;">
+				<input type="submit" name="redefinir" class="botao" value="Redefinir">
+				</div>
+
+				</form>
+			</div>
+		</div>
+	
+		
+	
+	</body>
+</html>
