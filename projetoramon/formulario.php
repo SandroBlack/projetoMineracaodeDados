f00be782bfb85bfe24834e28a9b7972d957aa825
Aquilla Silva Leite <aquilla11@hotmail.com>
2017-10-07 03:26:14 -0300

    SUPER HIPER MEGA AJAX 3.0
    ESSE AJAX TA FODA!

--- projetoramon/formulario.php
+++ projetoramon/formulario.php
@@ -1,96 +1,156 @@
 <?php include_once("conf/restricao.php");?>
 
 <!DOCTYPE html>
+
 <html lang="pt-br">
-<head>
-    <!-- ========== Título ============ -->
-    <title>Formulários</title>
 
-    <!-- ========== Metas ============ -->
-    <meta charset="utf-8">
+	<head>
+		<!-- ========== Título ============ -->
+		<title>Formulários</title>
 
-    <!-- ========== Links ============ -->
-    <link rel="stylesheet" href="css/formularios.css">
-    <link rel="icon" href="img/icon.png">
+		<!-- ========== Metas ============ -->
+		<meta charset="utf-8">
 
-    <!-- ========== Scripts ============ -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="js/sistema.js"></script>    
-</head>
+		<!-- ========== Links ============ -->
+		<link rel="stylesheet" href="css/formularios.css">
+		
+		<link rel="icon" href="img/icon.png">
 
-
-<body>
-
-
-    <!-- ======== Cabeçalho ========== -->
-    <header id="cabecalho">
-        <a href="menu-usuario.php"><img src="img/fddsdsadsa.png" style="width: 40px;height: 40px;margin: 14px 0px 0px 20px;" title="Volte a página do usuário"></a>
-        <button class="botao btnEnviar" id="enviar" name="enviar">Enviar</button>
-    </header><!-- ======== Fim do cabeçalho ==========-->
+		<!-- ========== Scripts ============ -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		
+		<script src="js/sistema.js"></script>    
+		
+	</head>
 
 
+	<body>
 
-    <!-- ====== container ====== -->
-    <div class="container">
-
-
-        <div class="area" align="center"><br>
-            <h1>Seu formulário</h1><br>
-        </div>
-
-
-
-        <!-- ============== Campo de Título ===============-->
-        <form class="titulo" >
-            <input type="text" name="titulo" id="titulo" placeholder="Título do formulário" maxlength="30"><br><br>
-            <input type="text" name="descricao" id="descricao" placeholder="Descrição do formulário" maxlength="30"><br><br>            
-        </form><br><br>
-
-
-
-        <!-- ============== Campo de resposta ===============-->
-        <div class="questoes" id="divPergunta">
+		<!-- ======== Cabeçalho ========== -->
+		<header id="cabecalho">
+		
+			<a href="menu-usuario.php"><img src="img/fddsdsadsa.png" style="width: 40px;height: 40px;margin: 14px 0px 0px 20px;" title="Volte a página do usuário"></a>
 			
-			<input type="text" name="pergunta" id="pergunta" placeholder="Pergunta">
-				
-			<select name="selecionar" id="selecionar">
-				
-				<option id="1" name="1" value="1">Tipo de resposta</option>
-				
-				<option id="2" name="2" value="2">Campo de texto</option>
-				
-				<option id="3" name="3" value="3">Multipla escolha</option>
-				
-			    <option id="4" name="4" value="4">Caixas de Seleção</option>
+			<button class="botao btnEnviar" id="enviar" name="enviar">Enviar</button>
 			
-			</select>
+		</header><!-- ======== Fim do cabeçalho ==========-->
+
+		<!-- ====== container ====== -->
+		<div id="meio" class="container">
+
+			<div class="area" align="center"><br>
+			
+				<h1>Seu formulário</h1><br>
 				
+			</div>
+
+			<!-- ============== Campo de Título ===============-->
+			<form class="titulo" >
+			
+				<input type="text" name="titulo" id="titulo" placeholder="Título do formulário" maxlength="30"><br><br>
+				
+				<input type="text" name="descricao" id="descricao" placeholder="Descrição do formulário" maxlength="30"><br><br> 
+				
+			</form>
+			
 			<br>
 			
-		</div>
+			<br>
+
+			<!-- ============== Campo de resposta ===============-->
+			<div class="questoes" id="divPergunta">
+				
+				<input type="text" name="pergunta" id="pergunta" placeholder="Pergunta">
+					
+				<select name="selecionar" id="selecionar">
+					
+					<option id="1" name="1" value="1">Tipo de resposta</option>
+					
+					<option id="2" name="2" value="2">Campo de texto</option>
+					
+					<option id="3" name="3" value="3">Multipla escolha</option>
+					
+					<option id="4" name="4" value="4">Caixas de Seleção</option>
+					
+					<option id="5" name="5" value="5">Campo para Data</option>
+					
+					<option id="6" name="6" value="6">Campo para E-mail</option>
+				
+				</select>
+					
+				<br>
+				
+			</div>
+			
+			<div class="questoes" id="divOpc">
+				
+			</div>
+			
+			<div class="questoes" id="divAdicionarOpc">
+				
+				<input type="button" name="adicionarOpc" id="adicionarOpc" title="Adicionar mais uma pergunta" value="   +   ">
+				
+				<input type="button" name="removerOpc" id="removerOpc" title="Exclui uma pergunta adicionada" value="   -   ">
+				
+				<br>
+				
+				<br>
+				
+			</div>
+
+			<div class="barra-inferior" id="menu">
+			
+				<div class="area-botoes">
+				
+					<img src="img/Defult Text.png" class="dublicar" id="duplicar" title="adicionar uma pergunta">      
+					
+					<img src="img/delete_remove_bin_icon-icons.com_72400.png" class="excluir" id="exclusao" title="excluir uma pergunta adicionada">
+					
+				</div>
+				
+			</div><!-- Fim da barra inferior -->
+			
+			<div class="barra-inferior" id="menuOpc">
+			
+				<div class="area-botoes">
+				
+					<img src="img/Defult Text.png" class="dublicar" id="finalizarOpc" title="Finalizar uma pergunta">  
+					
+					<img src="img/delete_remove_bin_icon-icons.com_72400.png" class="excluir" id="descartarOpc" title="Descartar a pergunta adicionada">
+					
+				</div>
+				
+			</div>
+			
 
 
 
-        <div class="barra-inferior" id="menu">
-            <div class="area-botoes">
-                <img src="img/Defult Text.png" class="dublicar" id="duplicar" title="adicionar uma pergunta">      
-                <img src="img/delete_remove_bin_icon-icons.com_72400.png" class="excluir" id="exclusao" title="excluir a ultima pergunta adicionada">
-            </div>
-        </div><!-- Fim da barra inferior -->
 
 
+		</div><!-- Fim do container -->
 
-
-
-    </div><!-- Fim do container -->
-
-    <div id="base" class="container"></div>		
-		<div id="escondido">			
-			<form name="EnviarFormulario" id="EnviarFormulario" method="post" action="recebeForm.php">
-				<input name="FormularioCompleto" id="FormularioCompleto" type="hidden" value="">
-			</form>
-		</div>
-
-</body>
+		<div id="base" class="container">
+		
+		</div>		
+		
+			<div id="escondido">	
+			
+				<form name="EnviarFormulario" id="EnviarFormulario" method="post" action="recebeForm.php">
+				
+					<input name="FormularioCompleto" id="FormularioCompleto" type="hidden" value="">
+					
+				</form>
+				
+			</div>
+			
+		<br>
+		
+		<br>
+		
+		<br>
+		
+		<br>
+		
+	</body>
 
 </html>
--- projetoramon/js/sistema.js
+++ projetoramon/js/sistema.js
@@ -1,205 +1,399 @@
-
 $(document).ready(function(){
-				
-				// ESCONDER DIV PARA ENVIO DO FORMULARIO COMPLETO
-				$("#escondido").css("display","none");
-				
-				// INICANDO ARRAY E CONTADOR ASSIM QUE ABRIR A PAGINA
-				arrayConteudo = [];
-				cont = 3;
+	
+	// ESCONDER DIV PARA ENVIO DO FORMULARIO COMPLETO
+	$("#escondido").css("display","none");
+	$("#divAdicionarOpc").css("display","none");
+	$("#menuOpc").css("display","none");
+	
+	// INICANDO ARRAYS E CONTADORES ASSIM QUE ABRIR A PAGINA
+	arrayConteudo = [];
+	cont = 3;
+	arrayOpc = [];
+	contOpc = 0;
+	
+	// INICANDO ARRAY COM VALORES PADRÕES E PRESERVANDO POSIÇÕES
+	arrayConteudo[0] = "";
+	arrayConteudo[1] = "";
+	arrayConteudo[2] = "<div><form>";
 
-				// INICANDO ARRAY COM VALORES PADRÕES E PRESERVANDO POSIÇÕES
-				arrayConteudo[0] = "";
-				arrayConteudo[1] = "";
-				arrayConteudo[2] = "<div><form>";
-
-				// INICANDO ARRAY COM AS PERGUNTAS PARA NÃO PODER REPETIR E TAMBEM EXCLUIR E NÃO EXCLUIR INDICES PRESERVADOS
-				arrayPergunta = [];
-				arrayPergunta[0] = "";
-				arrayPergunta[1] = "";
-				arrayPergunta[2] = "";
-				
-				$("#titulo").keyup(function(){
-					
-					var titulo = $("#titulo").val().toUpperCase();
-					
-					arrayConteudo[0] = "<h1 style='text-align:center;'>" + titulo + "</h1><br>";
-				
-					$('#base').text("");
-						
-					for(var i = 0; i < arrayConteudo.length; i++)
-						{
-							$("#base").append(arrayConteudo[i]);
-						}
-					
-				});
-				
-				$("#descricao").keyup(function(){
-					
-					var descricao = $("#descricao").val().toUpperCase();
-					
-					arrayConteudo[1] = "<h4 style='text-align:center;'>" + descricao + "</h4><br><br>";
-				
-					$('#base').text("");
-						
-					for(var i = 0; i < arrayConteudo.length; i++)
-						{
-							$("#base").append(arrayConteudo[i]);
-						}
-					
-				});
-				
-				$("#duplicar").click(function(){
-
-					var perguntaLowerCase = $('#pergunta').val();
-					
-					var perguntaUpperCase = perguntaLowerCase.toUpperCase();
-					
-					var tipoPergunta = $('#selecionar').val();
-
-					if(perguntaUpperCase == "" || tipoPergunta == 1)
-					{
-						alert("Por favor preencha todas as informações");
-					}
-					
-					else
-					{
-						
-						for(var i = 3; i <= arrayPergunta.length; i++)
-						{
-							if(perguntaUpperCase == arrayPergunta[i])
-							{
-								alert("Pergunta já adicionada");
-								return 0;
-								break;
-							}
-						}
-						
-						if (tipoPergunta == 2)
-							{
-								arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
-														"<label>" + perguntaUpperCase +": </label><br><br>" +
-														"<input type='text' style='height:30px;width:260px;font-size:16px;' id='resposta"+perguntaUpperCase+"' name='resposta"+perguntaUpperCase+"'>" +
-														"</div><br><br>";
-							}
-						if (tipoPergunta == 3)
-							{
-								arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
-														"<label>" + perguntaUpperCase +": </label><br><br>";
-								
-								var quantidade = prompt("Digite a quantidade de opções:");
-
-								for(var x = 0; x < quantidade; x++)
-								{
-									var valorMaisUm = x + 1;
-									
-									var nomeRadio = prompt("Escreva o valor da opção de Número: " + valorMaisUm);
-									
-									var nomeRadioUpperCase = nomeRadio.toUpperCase();
-									
-									var valorRadio = arrayConteudo[cont] + "<input type='radio' name='radio"+cont+"' value='"+nomeRadioUpperCase+"'>"+nomeRadioUpperCase+"<br>";
-									
-									arrayConteudo[cont] = valorRadio;
-								}
-								
-								var fecharDiv = arrayConteudo[cont] + "</div><br><br";
-								
-								arrayConteudo[cont] = fecharDiv;					
-							}
-						if (tipoPergunta == 4)
-							{
-								arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
-														"<label>" + perguntaUpperCase +": </label><br><br>";
-								
-								var quantidade = prompt("Digite a quantidade de opções:");
-
-								for(var x = 0; x < quantidade; x++)
-								{
-									var valorMaisUm = x + 1;
-									
-									var nomecheckbox = prompt("Escreva o valor da opção de Número: " + valorMaisUm);
-									
-									var nomecheckboxUpperCase = nomecheckbox.toUpperCase();
-									
-									var valorcheckbox = arrayConteudo[cont] + "<input type='checkbox' name='checkbox"+cont+"' value='"+nomecheckboxUpperCase+"'>"+nomecheckboxUpperCase+"<br>";
-									
-									arrayConteudo[cont] = valorcheckbox;
-								}
-								
-								var fecharDiv = arrayConteudo[cont] + "</div><br><br";
-								
-								arrayConteudo[cont] = fecharDiv;
-							}
-							
-					arrayPergunta[cont] = perguntaUpperCase;
-					
-					$("#base").append(arrayConteudo[cont]);
-					
-					cont++;
-					}
-				
-					$('#pergunta').val("");	
-
-					$('#selecionar').val(1);
-				
-				});
-
-				$("#menu").on('click', '.excluir', function(){
-					
-					if(arrayConteudo.length == 2)
-					{
-						alert("Não existe pergunta para remover");
-					}
-					
-					else
-					{	
-					
-						var mensagemInicial = "Deseja excluir qual das perguntas?\n";
-						
-						var modificandoArray = arrayPergunta.join("\n");
-						
-						var mensagemFinal = mensagemInicial + modificandoArray;
-						
-						var inicioExclusao = prompt(mensagemFinal);
-						
-						var inicioExclusaoUpperCase = inicioExclusao.toUpperCase();
-						
-						for(var i = 3; i <= arrayPergunta.length; i++)
-						{
-							if(inicioExclusaoUpperCase == arrayPergunta[i])
-							{
-								arrayConteudo.splice(i,1);
-								
-								arrayPergunta.splice(i,1);
-								
-								break;
-							}
-						}
-						
-						$('#base').text("");
-						
-						for(var i = 0; i < arrayConteudo.length; i++)
-						{
-							$("#base").append(arrayConteudo[i]);
-						}
-						
-						cont --;
-					}	
-
-				});
-
-				$("#enviar").click(function(){
-					
-					var arrayToString = arrayConteudo.join("|");
-					
-					var stringCompleto = arrayToString + "</form></div>";
-					
-					$('#FormularioCompleto').val(stringCompleto);
-					
-					$('#EnviarFormulario').submit();
-					
-					alert("Formulário Cadastrado com Sucesso");
-
-				});
+	// INICANDO ARRAY COM AS PERGUNTAS PARA NÃO PODER REPETIR E TAMBEM EXCLUIR E NÃO EXCLUIR INDICES PRESERVADOS
+	arrayPergunta = [];
+	arrayPergunta[0] = "";
+	arrayPergunta[1] = "";
+	arrayPergunta[2] = "";
+	
+	// INICIANDO VARIAVEL VAZIA PARA RECEBER O TIPO DE INPUT
+	tipoInput = "";
+	
+	$("#titulo").keyup(function(){
+		
+		var titulo = $("#titulo").val().toUpperCase();
+		
+		arrayConteudo[0] = "<h1 style='text-align:center;'>" + titulo + "</h1><br>";
+	
+		$('#base').text("");
 			
-			});
+		for (var i = 0; i < arrayConteudo.length; i++)
+			{
+				$("#base").append(arrayConteudo[i]);
+			}
+		
+	});
+	
+	$("#descricao").keyup(function(){
+		
+		var descricao = $("#descricao").val().toUpperCase();
+		
+		arrayConteudo[1] = "<h4 style='text-align:center;'>" + descricao + "</h4><br>";
+	
+		$('#base').text("");
+			
+		for (var i = 0; i < arrayConteudo.length; i++)
+			{
+				$("#base").append(arrayConteudo[i]);
+			}
+		
+	});
+	
+	$("#duplicar").click(function(){
+
+		var perguntaLowerCase = $('#pergunta').val();
+		
+		var perguntaUpperCase = perguntaLowerCase.toUpperCase();
+		
+		var tipoPergunta = $('#selecionar').val();
+
+		if (perguntaUpperCase == "" || tipoPergunta == 1)
+		{
+			alert("Por favor preencha todas as informações");
+		}
+		
+		else
+		{
+			
+			for (var i = 3; i <= arrayPergunta.length; i++)
+			{
+				if (perguntaUpperCase == arrayPergunta[i])
+				{
+					alert("Pergunta já adicionada");
+					return 0;
+					break;
+				}
+			}
+			
+			if (tipoPergunta == 2)
+			{
+					arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
+											"<label>" + perguntaUpperCase +": </label>" +
+											"<input type='text' style='height:30px;width:260px;font-size:16px;' id='resposta"+perguntaUpperCase+"' name='resposta"+perguntaUpperCase+"'>" +
+											"</div><br>";
+											
+			}
+			if (tipoPergunta == 3)
+			{
+					tipoInput = "radio";
+					
+					arrayOpc[contOpc] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'> <input type='text' id='respostaOpc" + contOpc + "' name='respostaOpc' placeholder='Digite o valor'>";
+					
+					var pularLinha = "<br><br>";
+					
+					$("#divOpc").append(arrayOpc[contOpc] + pularLinha);
+					
+					$("#divAdicionarOpc").css("display","block");
+					
+					$("#menu").css("display","none");
+					
+					$("#menuOpc").css("display","block");
+					
+					$('#pergunta').val(perguntaUpperCase);	
+
+					$('#selecionar').val(3);
+					
+					contOpc++;
+					
+					return 0;
+					
+			}
+			if (tipoPergunta == 4)
+			{
+					tipoInput = "checkbox";
+					
+					arrayOpc[contOpc] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'> <input type='text' id='respostaOpc" + contOpc + "' name='respostaOpc' placeholder='Digite o valor'>";
+					
+					var pularLinha = "<br><br>";
+					
+					$("#divOpc").append(arrayOpc[contOpc] + pularLinha);
+					
+					$("#divAdicionarOpc").css("display","block");
+					
+					$("#menu").css("display","none");
+					
+					$("#menuOpc").css("display","block");
+					
+					$('#pergunta').val(perguntaUpperCase);	
+
+					$('#selecionar').val(3);
+					
+					contOpc++;
+					
+					return 0;
+					
+					
+			}
+			if (tipoPergunta == 5)
+			{
+					arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
+											"<label>" + perguntaUpperCase +": </label>" +
+											"<input type='date' id='resposta"+perguntaUpperCase+"' name='resposta"+perguntaUpperCase+"'>" +
+											"</div><br>";
+											
+			}
+			if (tipoPergunta == 6)
+			{
+					arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
+											"<label>" + perguntaUpperCase +": </label>" +
+											"<input type='email' id='resposta"+perguntaUpperCase+"' name='resposta"+perguntaUpperCase+"'>" +
+											"</div><br>";
+											
+			}
+				
+			arrayPergunta[cont] = perguntaUpperCase;
+			$("#base").append(arrayConteudo[cont]);
+				
+			cont++;
+						
+		}
+	
+		$('#pergunta').val("");	
+
+		$('#selecionar').val(1);
+	
+	});
+
+	$("#menu").on('click', '.excluir', function(){
+		
+		if (arrayConteudo.length == 3)
+		{
+			alert("Não existe pergunta para remover");
+		}
+
+		else
+		{					
+			var modificandoArray = "";
+			var mensagemInicial = "Deseja excluir qual das perguntas?\n";
+			
+			for (var i = 3; i < arrayPergunta.length; i++)
+			{
+				
+				modificandoArray += arrayPergunta[i] + "\n";
+			}
+			var mensagemFinal = mensagemInicial + modificandoArray;
+			
+			var inicioExclusao = prompt(mensagemFinal);
+			
+			var inicioExclusaoUpperCase = inicioExclusao.toUpperCase();
+			
+			if (inicioExclusaoUpperCase == "")
+			{
+				alert("Necessário digitar a pergunta");
+				return 0;
+			}
+			
+			else
+			{	
+				for (var i = 3; i <= arrayPergunta.length; i++)
+				{
+					if (inicioExclusaoUpperCase == arrayPergunta[i])
+					{
+						arrayConteudo.splice(i,1);
+						
+						arrayPergunta.splice(i,1);
+						
+						break;
+					}
+					
+					if (i == arrayPergunta.length)
+					{
+						alert("Pergunta não encontrada");
+						return 0;
+					}
+				}
+				
+				$('#base').text("");
+				
+				for (var i = 0; i < arrayConteudo.length; i++)
+				{
+					$("#base").append(arrayConteudo[i]);
+				}
+				
+				cont --;
+			}
+		}	
+
+	});
+	
+	$(document).on('click', '#adicionarOpc', function(){
+		
+		arrayOpc[contOpc] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'> <input type='text' id='respostaOpc" + contOpc + "' name='respostaOpc" + contOpc + "'" +
+							"placeholder='Digite o valor'>";
+					
+		var pularLinha = "<br><br>";
+					
+		$("#divOpc").append(arrayOpc[contOpc] + pularLinha);
+				
+		contOpc++;
+		
+	});
+	
+	$(document).on('click', '#removerOpc', function(){
+		
+		
+		
+		if (arrayOpc.length == 1)
+		{
+			alert("Se deseja cancelar a pergunta, clique em 'Descartar'");
+		}
+		else
+		{
+			
+			for (var i = 0; i < contOpc; i++)
+			{
+				var recebeOpc = $("#respostaOpc" + i + "").val();
+				var recebeOpcUpperCase = recebeOpc.toUpperCase();
+				
+				arrayOpc[i] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'> <input type='text' id='respostaOpc" + i + "' name='respostaOpc" + i + "'" +
+							  "value='"+ recebeOpcUpperCase +"'>";
+			}
+
+			var removeOpc = arrayOpc.length - 1;
+				
+			arrayOpc.splice(removeOpc, 1);
+				
+			var pularLinha = "<br><br>";
+		
+			$('#divOpc').text("");
+					
+			for (var i = 0; i < arrayOpc.length; i++)
+			{
+				$("#divOpc").append(arrayOpc[i] + pularLinha);
+			}			
+					
+			contOpc--;
+		}
+		
+	});
+	
+	$("#finalizarOpc").click(function(){
+
+		if(arrayOpc.length == 1)
+		{
+			alert("É necessário adicionar uma outra opção para esse tipo de pergunta");
+			
+			return 0;
+		}
+		else	
+		{
+			var perguntaLowerCase = $('#pergunta').val();
+			
+			var perguntaUpperCase = perguntaLowerCase.toUpperCase();	
+			
+			for (var i = 0; i < contOpc; i++)
+			{
+				var recebeOpc = $("#respostaOpc" + i + "").val();
+				
+				var recebeOpcUpperCase = recebeOpc.toUpperCase();
+				
+				if (recebeOpcUpperCase == "")
+				{
+					alert("Por favor, preencha os campos corretamente");
+					
+					return 0;
+				}
+				else
+				{
+					arrayOpc[i] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='"+ recebeOpcUpperCase +"' >" + recebeOpcUpperCase +"<br>";
+				}
+			}	
+		}
+		
+		arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
+								"<label>" + perguntaUpperCase +": </label><br>";
+
+		for (var i = 0; i < contOpc; i++)
+		{
+			arrayConteudo[cont] += arrayOpc[i];
+		}
+		
+		arrayPergunta[cont] = perguntaUpperCase;
+		
+		arrayConteudo[cont] += "</div><br>";
+		
+		$("#base").append(arrayConteudo[cont]);
+				
+		cont++;
+		
+		arrayOpc.splice(0, arrayOpc.length);
+		
+		contOpc = 0;
+	
+		$('#pergunta').val("");	
+
+		$('#selecionar').val(1);
+		
+		$('#pergunta').val("");	
+
+		$('#selecionar').val(1);
+		
+		$("#divOpc").text("");
+		
+		$("#divAdicionarOpc").css("display","none");
+					
+		$("#menu").css("display","block");
+					
+		$("#menuOpc").css("display","none");
+	
+	});
+	
+	$("#descartarOpc").click(function(){
+		
+		$('#pergunta').val("");	
+
+		$('#selecionar').val(1);
+		
+		$("#divOpc").text("");
+		
+		$("#divAdicionarOpc").css("display","none");
+					
+		$("#menu").css("display","block");
+					
+		$("#menuOpc").css("display","none");
+		
+	});
+	
+	$("#enviar").click(function(){
+		
+		var verificaTitulo = $("#titulo").val();
+		
+		var verificaDescricao = $("#descricao").val();
+		
+		if (arrayConteudo.length == 3 || verificaTitulo == "" || verificaDescricao == "")
+		{
+			alert("Formulário incompleto");
+		}
+		
+		else
+		{				
+			var arrayToString = arrayConteudo.join("|");
+			
+			var stringCompleto = arrayToString + "</form></div>";
+			
+			$('#FormularioCompleto').val(stringCompleto);
+			
+			$('#EnviarFormulario').submit();
+			
+			alert("Formulário Cadastrado com Sucesso");
+		}
+
+	});
+
+});
