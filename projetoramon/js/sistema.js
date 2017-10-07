133aaa2c606100819ce8fbb3d0e46f0d4bdaf9ab
Aquilla Silva Leite <aquilla11@hotmail.com>
2017-10-07 03:45:41 -0300

    attaatttattattatat
    atttatatatataatat

--- projetoramon/js/sistema.js
+++ projetoramon/js/sistema.js
@@ -1,28 +1,41 @@
 $(document).ready(function(){
 	
-	// ESCONDER DIV PARA ENVIO DO FORMULARIO COMPLETO
+	// ESCONDER DIV 
+	
 	$("#escondido").css("display","none");
+	
 	$("#divAdicionarOpc").css("display","none");
+	
 	$("#menuOpc").css("display","none");
 	
 	// INICANDO ARRAYS E CONTADORES ASSIM QUE ABRIR A PAGINA
+	
 	arrayConteudo = [];
+	
 	cont = 3;
+	
 	arrayOpc = [];
+	
 	contOpc = 0;
 	
 	// INICANDO ARRAY COM VALORES PADRÕES E PRESERVANDO POSIÇÕES
+	
 	arrayConteudo[0] = "";
+	
 	arrayConteudo[1] = "";
+	
 	arrayConteudo[2] = "<div><form>";
-
-	// INICANDO ARRAY COM AS PERGUNTAS PARA NÃO PODER REPETIR E TAMBEM EXCLUIR E NÃO EXCLUIR INDICES PRESERVADOS
+	
 	arrayPergunta = [];
+	
 	arrayPergunta[0] = "";
+	
 	arrayPergunta[1] = "";
+	
 	arrayPergunta[2] = "";
 	
 	// INICIANDO VARIAVEL VAZIA PARA RECEBER O TIPO DE INPUT
+	
 	tipoInput = "";
 	
 	$("#titulo").keyup(function(){
@@ -34,9 +47,9 @@ $(document).ready(function(){
 		$('#base').text("");
 			
 		for (var i = 0; i < arrayConteudo.length; i++)
-			{
-				$("#base").append(arrayConteudo[i]);
-			}
+		{
+			$("#base").append(arrayConteudo[i]);
+		}
 		
 	});
 	
@@ -49,125 +62,133 @@ $(document).ready(function(){
 		$('#base').text("");
 			
 		for (var i = 0; i < arrayConteudo.length; i++)
-			{
-				$("#base").append(arrayConteudo[i]);
-			}
+		{
+			$("#base").append(arrayConteudo[i]);
+		}
 		
 	});
 	
 	$("#duplicar").click(function(){
-
-		var perguntaLowerCase = $('#pergunta').val();
 		
-		var perguntaUpperCase = perguntaLowerCase.toUpperCase();
-		
-		var tipoPergunta = $('#selecionar').val();
-
-		if (perguntaUpperCase == "" || tipoPergunta == 1)
+		if(cont == 13)
 		{
-			alert("Por favor preencha todas as informações");
+			alert("Limite de perguntas atingido");
 		}
-		
 		else
 		{
+			var perguntaLowerCase = $('#pergunta').val();
 			
-			for (var i = 3; i <= arrayPergunta.length; i++)
+			var perguntaUpperCase = perguntaLowerCase.toUpperCase();
+			
+			var tipoPergunta = $('#selecionar').val();
+
+			if (perguntaUpperCase == "" || tipoPergunta == 1)
 			{
-				if (perguntaUpperCase == arrayPergunta[i])
+				alert("Por favor preencha todas as informações");
+			}
+			else
+			{
+				
+				for (var i = 3; i <= arrayPergunta.length; i++)
 				{
-					alert("Pergunta já adicionada");
-					return 0;
-					break;
-				}
-			}
-			
-			if (tipoPergunta == 2)
-			{
-					arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
-											"<label>" + perguntaUpperCase +": </label>" +
-											"<input type='text' style='height:30px;width:260px;font-size:16px;' id='resposta"+perguntaUpperCase+"' name='resposta"+perguntaUpperCase+"'>" +
-											"</div><br>";
-											
-			}
-			if (tipoPergunta == 3)
-			{
-					tipoInput = "radio";
-					
-					arrayOpc[contOpc] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'> <input type='text' id='respostaOpc" + contOpc + "' name='respostaOpc' placeholder='Digite o valor'>";
-					
-					var pularLinha = "<br><br>";
-					
-					$("#divOpc").append(arrayOpc[contOpc] + pularLinha);
-					
-					$("#divAdicionarOpc").css("display","block");
-					
-					$("#menu").css("display","none");
-					
-					$("#menuOpc").css("display","block");
-					
-					$('#pergunta').val(perguntaUpperCase);	
-
-					$('#selecionar').val(3);
-					
-					contOpc++;
-					
-					return 0;
-					
-			}
-			if (tipoPergunta == 4)
-			{
-					tipoInput = "checkbox";
-					
-					arrayOpc[contOpc] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'> <input type='text' id='respostaOpc" + contOpc + "' name='respostaOpc' placeholder='Digite o valor'>";
-					
-					var pularLinha = "<br><br>";
-					
-					$("#divOpc").append(arrayOpc[contOpc] + pularLinha);
-					
-					$("#divAdicionarOpc").css("display","block");
-					
-					$("#menu").css("display","none");
-					
-					$("#menuOpc").css("display","block");
-					
-					$('#pergunta').val(perguntaUpperCase);	
-
-					$('#selecionar').val(3);
-					
-					contOpc++;
-					
-					return 0;
-					
-					
-			}
-			if (tipoPergunta == 5)
-			{
-					arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
-											"<label>" + perguntaUpperCase +": </label>" +
-											"<input type='date' id='resposta"+perguntaUpperCase+"' name='resposta"+perguntaUpperCase+"'>" +
-											"</div><br>";
-											
-			}
-			if (tipoPergunta == 6)
-			{
-					arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
-											"<label>" + perguntaUpperCase +": </label>" +
-											"<input type='email' id='resposta"+perguntaUpperCase+"' name='resposta"+perguntaUpperCase+"'>" +
-											"</div><br>";
-											
-			}
-				
-			arrayPergunta[cont] = perguntaUpperCase;
-			$("#base").append(arrayConteudo[cont]);
-				
-			cont++;
+					if (perguntaUpperCase == arrayPergunta[i])
+					{
+						alert("Pergunta já adicionada");
 						
-		}
-	
-		$('#pergunta').val("");	
+						return 0;
+					}
+				}
+				
+				if (tipoPergunta == 2)
+				{
+						arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
+												"<label>" + perguntaUpperCase +": </label>" +
+												"<input type='text' style='height:30px;width:260px;font-size:16px;' id='resposta"+perguntaUpperCase+"' name='resposta"+perguntaUpperCase+"'>" +
+												"</div><br>";
+												
+				}
+				if (tipoPergunta == 3)
+				{
+						tipoInput = "radio";
+						
+						arrayOpc[contOpc] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'>" + 
+											"<input type='text' id='respostaOpc" + contOpc + "' name='respostaOpc' placeholder='Digite o valor'>";
+						
+						var pularLinha = "<br><br>";
+						
+						$("#divOpc").append(arrayOpc[contOpc] + pularLinha);
+						
+						$("#divAdicionarOpc").css("display","block");
+						
+						$("#menu").css("display","none");
+						
+						$("#menuOpc").css("display","block");
+						
+						$('#pergunta').val(perguntaUpperCase);	
 
-		$('#selecionar').val(1);
-	
+						$('#selecionar').val(3);
+						
+						contOpc++;
+						
+						return 0;
+						
+				}
+				if (tipoPergunta == 4)
+				{
+						tipoInput = "checkbox";
+						
+						arrayOpc[contOpc] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'>" +
+											"<input type='text' id='respostaOpc" + contOpc + "' name='respostaOpc' placeholder='Digite o valor'>";
+						
+						var pularLinha = "<br><br>";
+						
+						$("#divOpc").append(arrayOpc[contOpc] + pularLinha);
+						
+						$("#divAdicionarOpc").css("display","block");
+						
+						$("#menu").css("display","none");
+						
+						$("#menuOpc").css("display","block");
+						
+						$('#pergunta').val(perguntaUpperCase);	
+
+						$('#selecionar').val(3);
+						
+						contOpc++;
+						
+						return 0;
+						
+						
+				}
+				if (tipoPergunta == 5)
+				{
+						arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
+												"<label>" + perguntaUpperCase +": </label>" +
+												"<input type='date' id='resposta"+perguntaUpperCase+"' name='resposta"+perguntaUpperCase+"'>" +
+												"</div><br>";
+												
+				}
+				if (tipoPergunta == 6)
+				{
+						arrayConteudo[cont] =	"<div style='margin-left:15px;'>" +
+												"<label>" + perguntaUpperCase +": </label>" +
+												"<input type='email' id='resposta"+perguntaUpperCase+"' name='resposta"+perguntaUpperCase+"'>" +
+												"</div><br>";
+												
+				}
+					
+				arrayPergunta[cont] = perguntaUpperCase;
+				
+				$("#base").append(arrayConteudo[cont]);
+					
+				cont++;
+							
+			}
+		
+			$('#pergunta').val("");	
+
+			$('#selecionar').val(1);
+		}
 	});
 
 	$("#menu").on('click', '.excluir', function(){
@@ -176,7 +197,6 @@ $(document).ready(function(){
 		{
 			alert("Não existe pergunta para remover");
 		}
-
 		else
 		{					
 			var modificandoArray = "";
@@ -198,7 +218,6 @@ $(document).ready(function(){
 				alert("Necessário digitar a pergunta");
 				return 0;
 			}
-			
 			else
 			{	
 				for (var i = 3; i <= arrayPergunta.length; i++)
@@ -211,7 +230,6 @@ $(document).ready(function(){
 						
 						break;
 					}
-					
 					if (i == arrayPergunta.length)
 					{
 						alert("Pergunta não encontrada");
@@ -234,8 +252,8 @@ $(document).ready(function(){
 	
 	$(document).on('click', '#adicionarOpc', function(){
 		
-		arrayOpc[contOpc] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'> <input type='text' id='respostaOpc" + contOpc + "' name='respostaOpc" + contOpc + "'" +
-							"placeholder='Digite o valor'>";
+		arrayOpc[contOpc] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'>" +
+							"<input type='text' id='respostaOpc" + contOpc + "' name='respostaOpc" + contOpc + "' placeholder='Digite o valor'>";
 					
 		var pularLinha = "<br><br>";
 					
@@ -246,8 +264,6 @@ $(document).ready(function(){
 	});
 	
 	$(document).on('click', '#removerOpc', function(){
-		
-		
 		
 		if (arrayOpc.length == 1)
 		{
@@ -261,8 +277,8 @@ $(document).ready(function(){
 				var recebeOpc = $("#respostaOpc" + i + "").val();
 				var recebeOpcUpperCase = recebeOpc.toUpperCase();
 				
-				arrayOpc[i] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'> <input type='text' id='respostaOpc" + i + "' name='respostaOpc" + i + "'" +
-							  "value='"+ recebeOpcUpperCase +"'>";
+				arrayOpc[i] = "<input type='"+ tipoInput +"' name='"+ tipoInput + cont + "' value='' disabled='true'>" +
+							  "<input type='text' id='respostaOpc" + i + "' name='respostaOpc" + i + "' value='"+ recebeOpcUpperCase +"'>";
 			}
 
 			var removeOpc = arrayOpc.length - 1;
