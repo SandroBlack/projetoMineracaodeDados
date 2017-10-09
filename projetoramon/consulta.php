6e89364f9d18115366642d855ab4db6a8b987b0c
weverson10 <weverson_acioly@hotmail.com>
2017-10-08 21:00:08 -0300

    atualização seus puto

---
+++ projetoramon/consulta.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html  lang="pt-br">
+
+<head>
+	<title>Consulta</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/consulta.css">
+	<link rel="icon" href="img/icon.png">
+</head>
+
+<body>
+	<!-- ======== Cabeçalho ========== -->
+	<header id="cabecalho">
+
+		<a href="menu-usuario.php">
+
+			<img src="img/fddsdsadsa.png" style="width: 40px;height: 40px;margin: 14px 0px 0px 20px;" title="Volte ao menu do usuário">
+
+		</a>
+
+	</header><!-- ======== Fim do cabeçalho ==========-->
+
+
+	<div class="container">
+
+		<h1 style="text-align: center;">Seus Formulários</h1><br><br>
+
+		<table style="width: 810px;margin: 10px auto 0px auto;" cellspacing="10" >
+
+			<tr>
+				<th style="width: 270px;text-align: center;">Título</th>
+				<th style="width: 270px;text-align: center;">Descrição</th>
+				<th style="width: 270px;text-align: center;">Data</th>
+			</tr>
+
+			<tr>
+				<td>área Tí</td>
+				<td>área Des</td>
+				<td>área Da</td>
+			</tr>
+			<tr>
+				<td>área Tí</td>
+				<td>área Des</td>
+				<td>área Da</td>
+			</tr>
+
+		</table>
+
+	</div>
+
+</body>
+
+
+</html>
