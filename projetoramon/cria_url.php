58c3c9c38f9dce48ec450bf91127292ca5df4275
Aquilla Silva Leite <aquilla11@hotmail.com>
2017-10-13 04:12:59 -0300

    att
    att

---
+++ projetoramon/cria_url.php
@@ -0,0 +1,82 @@
+<?php 
+
+	function gerarUrl(){
+		
+		$disponivel = true;
+		
+		while($disponivel)
+		{
+			$retorno = "";
+			
+			$tamanho = 30;
+			
+			$maiusculas = true;
+			
+			$numeros = true;
+			
+			$simbolos = true;
+			
+			$lmin = 'abcdefghijklmnopqrstuvwxyz';
+			
+			$lmai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			
+			$num = '1234567890';
+			
+			$simb = '!@#$%-';
+			
+		
+			$retorno = '';
+			
+			$caracteres = '';
+			
+	
+			$caracteres .= $lmin;
+			
+			if ($maiusculas) $caracteres .= $lmai;
+			
+			if ($numeros) $caracteres .= $num;
+			
+			if ($simbolos) $caracteres .= $simb;
+			
+
+			$len = strlen($caracteres);
+			
+			for ($n = 1; $n <= $tamanho; $n++) 
+			{
+				$rand = mt_rand(1, $len);
+
+				$retorno .= $caracteres[$rand-1];
+			}
+			
+			$urlFinal = "http://localhost/projetoMineracaodeDados/projetoramon/form_resposta.php?form_conteudo=" .$retorno;
+			
+			try
+			{
+				$pdo = conectar();
+				
+				$sql = "SELECT link_conteudo FROM link WHERE link_conteudo = :link_conteudo";
+				
+				$select = $pdo->prepare($sql);			
+				
+				$select->bindValue(":link_conteudo", $urlFinal);
+				
+				$select->execute();
+				
+				$linha = $select->rowCount();
+				
+				if ($linha == 0)
+				{
+					$disponivel == false;
+				}
+			} 
+		
+			catch(PDOException $e)
+			{
+				echo "Erro: " . $e->getMessage();
+			}
+		};
+		
+		return $urlFinal;			
+	}
+
+?>
