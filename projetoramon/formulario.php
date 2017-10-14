9fc0bf0166efe4aac8bdf1ea5ecbc29aa7d203d8
Aquilla Silva Leite <aquilla11@hotmail.com>
2017-10-14 18:01:44 -0300

    att
    att

--- projetoramon/formulario.php
+++ projetoramon/formulario.php
@@ -92,7 +92,7 @@
 					
 					<option id="2" name="2" value="2">Campo para resposta curta</option>
 					
-					<option id="3" name="3" value="3">Caixas de Seleção</option>
+					<!-- <option id="3" name="3" value="3">Caixas de Seleção</option> -->
 					
 					<option id="4" name="4" value="4">Multipla escolha</option>
 					
